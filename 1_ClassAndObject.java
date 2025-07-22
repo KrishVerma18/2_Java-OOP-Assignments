cd28c3d8455de09848a6a241c62942f4cd78368d
Krish Verma <kishuverma2005@gmail.com>
2025-07-22 18:10:20 +0530

    Added 1_ClassAndObject.java to demonstrate class and object creation
    Simple Java program showing how to define a class and create an object.

---
+++ 1_ClassAndObject.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name = "Krish";
+    int rollNo = 101;
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println("Name: " + s.name);
+        System.out.println("Roll No: " + s.rollNo);
+    }
+}
