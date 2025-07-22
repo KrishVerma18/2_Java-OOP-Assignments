ea6815dee95adf7dc7f32e74d0e5364b8cf97d53
Krish Verma <kishuverma2005@gmail.com>
2025-07-22 20:02:13 +0530

    Added 8d_BuiltInExceptions.java to demonstrate common built-in exceptions
    Examples of ArrayIndexOutOfBoundsException and NullPointerException with try-catch blocks.

---
+++ 8d_BuildInExceptions.java
@@ -0,0 +1,20 @@
+public class BuiltInExceptions {
+    public static void main(String[] args) {
+
+        // ArrayIndexOutOfBoundsException
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught: " + e);
+        }
+
+        // NullPointerException
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught: " + e);
+        }
+    }
+}
